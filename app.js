db749bdef3e6016cd1435aa5aae77e20e5c4f5f5
NicoleDezoraive <49281896+NicoleDezoraive@users.noreply.github.com>
2023-02-06 14:37:32 +0200

    Adding pages, NAV and links

--- app.js
+++ app.js
@@ -12,22 +12,37 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.get('/', (req, res) => {
+  res.render('home', {
+    paragraphTextHome: homeStartingContent,
+  });
 });
+
+app.get('/contact', (req, res) => {
+  res.render('contact', {
+    paragraphTextContact: contactContent,
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    paragraphTextAbout: aboutContent,
+  });
+});
+
+
+
+
+
+
+
+
+
+    app.listen(3000, function() {
+      console.log("Server started on port 3000");
+    });
