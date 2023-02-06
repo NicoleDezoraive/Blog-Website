13cacfdfeb36857733ca9aa54d184feb457e1fc5
NicoleDezoraive <49281896+NicoleDezoraive@users.noreply.github.com>
2023-02-06 18:20:07 +0200

    Add write a post and show on the home page

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+let posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -20,6 +21,7 @@ app.use(express.static("public"));
 app.get('/', (req, res) => {
   res.render('home', {
     paragraphTextHome: homeStartingContent,
+    newListPosts: posts
   });
 });
 
@@ -35,6 +37,18 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/compose', (req, res) => {
+  res.render('compose');
+});
+
+app.post('/compose', (req, res) => {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
+});
 
 
 
@@ -43,6 +57,7 @@ app.get('/about', (req, res) => {
 
 
 
-    app.listen(3000, function() {
-      console.log("Server started on port 3000");
-    });
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
